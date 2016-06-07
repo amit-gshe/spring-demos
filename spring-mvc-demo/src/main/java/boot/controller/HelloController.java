096d342bf56513e918209dbbf79eecfa21376c09
gshe <gshe@cxria.com>
2016-06-07 16:40:26 +0800

    add xml support!

--- spring-mvc-demo/src/main/java/boot/controller/HelloController.java
+++ spring-mvc-demo/src/main/java/boot/controller/HelloController.java
@@ -1,13 +1,31 @@
 package boot.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.thoughtworks.xstream.XStream;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello";
-	}
+  @Autowired
+  XStream xStream;
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "hello";
+  }
+
+  @SuppressWarnings("rawtypes")
+  @RequestMapping(path = "/xml", method = RequestMethod.POST)
+  @ResponseBody
+  public Map request(@RequestBody String params) {
+    return (Map) xStream.fromXML(params);
+  }
 }
