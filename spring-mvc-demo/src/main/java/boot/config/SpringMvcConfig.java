6ab64454ca53e7b52167aade151422d1cdae7c4f
Amit <scybhe@163.com>
2016-06-09 01:15:14 +0800

    organize the imports

--- spring-mvc-demo/src/main/java/boot/config/SpringMvcConfig.java
+++ spring-mvc-demo/src/main/java/boot/config/SpringMvcConfig.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
