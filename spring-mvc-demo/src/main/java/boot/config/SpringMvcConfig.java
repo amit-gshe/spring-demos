ac082e67a5befa3f21735e6472f078725c2deae2
Amit <scybhe@163.com>
2016-08-03 23:48:17 +0800

    format code

--- spring-mvc-demo/src/main/java/boot/config/SpringMvcConfig.java
+++ spring-mvc-demo/src/main/java/boot/config/SpringMvcConfig.java
@@ -10,21 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class SpringMvcConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 
-	    return new EmbeddedServletContainerCustomizer() {
-	        @Override
-	        public void customize(ConfigurableEmbeddedServletContainer container) {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
 
-	            ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-	            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error");
-	            ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
-
-	            container.addErrorPages(error401Page, error404Page, error500Page);
-	        }
-	    };
+				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error");
+				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+				container.addErrorPages(error401Page, error404Page, error500Page);
+			}
+		};
 	}
 
 }
